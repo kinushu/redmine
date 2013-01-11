dd1103a2855917d72327d0b8774b16dfb18f8cac
shuhei kinukawa <kinushu@gmail.com>
2013-01-12 01:03:07 +0900

    Prepare for heroku

--- config/application.rb
+++ config/application.rb
@@ -48,6 +48,8 @@ module RedmineApp
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.action_mailer.perform_deliveries = false
 
     config.session_store :cookie_store, :key => '_redmine_session'
