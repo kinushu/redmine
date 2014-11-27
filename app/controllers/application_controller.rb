359b6a89aaa792d60c863122ba592805b29f8d41
Shuhei Kinukawa <kinushu@gmail.com>
2014-11-27 23:30:53 +0900

    Cancel basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,6 @@ require 'cgi'
 class Unauthorized < Exception; end
 
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] 
 
   include Redmine::I18n
   include Redmine::Pagination
